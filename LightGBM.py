3944046bf8d826208e4efacb9bf13db5750c78b5
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-03-18 13:42:35 +0100

    Update LightGBM.py

--- LightGBM.py
+++ LightGBM.py
@@ -1,1 +1,21 @@
 import lightgbm as lgb
+
+### initial parameter list :
+  param = {
+    'bagging_freq': 4,
+    'bagging_fraction': 0.4,
+    'boost_from_average':'false',
+    'boost': 'gbdt',
+    'feature_fraction': 0.04,
+    'learning_rate': 0.001,
+    'max_depth': -1,
+    'metric':'auc',
+    'min_data_in_leaf': 50,
+    'min_sum_hessian_in_leaf': 10.0,
+    'num_leaves': 10,
+    'num_threads': 8,
+    'tree_learner': 'serial',
+    'objective': 'binary',
+    'verbosity': 1
+        
+    }
