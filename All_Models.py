38d9512727647bf1b2834ccca0ad800a12ee1054
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-03-19 23:25:30 +0100

    Update All_Models.py

--- All_Models.py
+++ All_Models.py
@@ -12,7 +12,21 @@ Algos = [
      ensemble.BaggingClassifier(),
      ensemble.ExtraTreesClassifier(),
      ensemble.GradientBoostingClassifier(),
-     ensemble.RandomForestClassifier()
+     ensemble.RandomForestClassifier(), #Gaussian Processes
+     gaussian_process.GaussianProcessClassifier(),
+    
+#GLM 
+    linear_model.LogisticRegressionCV(),
+    linear_model.PassiveAggressiveClassifier(),
+    linear_model.RidgeClassifierCV(),
+    linear_model.SGDClassifier()
+    linear_model.Perceptron(),
+    
+#Navies Bayes
+    naive_bayes.BernoulliNB(),
+    naive_bayes.GaussianNB()
+    
+
   
  
     
