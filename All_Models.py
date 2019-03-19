4ba5ed869716deebfc4df46db50c9a79b6a81ffd
Wajdi Ben Saad <6123092+WajdiBenSaad@users.noreply.github.com>
2019-03-19 23:25:10 +0100

    Update All_Models.py

--- All_Models.py
+++ All_Models.py
@@ -1,1 +1,20 @@
 
+############################
+## ALL MODELS
+############################
+Algos = [
+  
+   #Xgboost
+    XGBClassifier() ,
+  
+   #Ensemble Methods
+     ensemble.AdaBoostClassifier(),
+     ensemble.BaggingClassifier(),
+     ensemble.ExtraTreesClassifier(),
+     ensemble.GradientBoostingClassifier(),
+     ensemble.RandomForestClassifier()
+  
+ 
+    
+      
+    ]
